ba136e1bcf37d8d26493893b61a36b86d2bf3ca1
HideOnShroud <natroshviliguga5@gmail.com>
2024-05-18 22:13:56 +0400

    input id change

--- client/src/pages/AddPage.tsx
+++ client/src/pages/AddPage.tsx
@@ -192,18 +192,18 @@ const AddPage = () => {
         <>
             <Navbar title="Product Add" button1Action="/" button1="Cancel" button2="Save" button2Action={addProduct} />
             <div className="bg-neutral-50 gap-5 pl-10 pt-20 flex justify-center pr-10 w-full h-[100vh]">
-                <form action="" id="product-form" className="flex shadow-md w-5/6 lg:w-1/2 justify-center bg-neutral-100 rounded-xl h-max pb-5 mt-10" method="post">
+                <form action="" id="#product-form" className="flex shadow-md w-5/6 lg:w-1/2 justify-center bg-neutral-100 rounded-xl h-max pb-5 mt-10" method="post">
                     <div className="flex flex-col gap-3 w-2/3 pt-5 pb-5">
 
                         <label htmlFor="sku" >SKU:</label>
-                        <input type="text" onClick={() => onClick('sku')} onChange={handleChange} className={inputStyle('sku')} name="sku" id="sku" />
+                        <input type="text" onClick={() => onClick('sku')} onChange={handleChange} className={inputStyle('sku')} name="sku" id="#sku" />
                         <label htmlFor="name" >Name:</label>
-                        <input type="text" onChange={handleChange} onClick={() => onClick('name')} className={inputStyle('name')} name="name" id="name" />
+                        <input type="text" onChange={handleChange} onClick={() => onClick('name')} className={inputStyle('name')} name="name" id="#name" />
                         <label htmlFor="price" >Price ($):</label>
-                        <input type="number" onChange={handleChange} onClick={() => onClick('price')} className={inputStyle('price')} name="price" id="price" />
+                        <input type="number" onChange={handleChange} onClick={() => onClick('price')} className={inputStyle('price')} name="price" id="#price" />
                         <label htmlFor="productType" >Product Type:</label>
 
-                        <select id="productType" onChange={(e) => { setSelect(e.target.value), handleChange }} className="border-2 border-cyan-900 rounded-md shadow-md w-full self-center">
+                        <select id="#productType" onChange={(e) => { setSelect(e.target.value), handleChange }} className="border-2 border-cyan-900 rounded-md shadow-md w-full self-center">
                             <option value=""></option>
                             <option value="Book">Book</option>
                             <option value="DVD">DvD</option>
@@ -213,23 +213,23 @@ const AddPage = () => {
                             ?
                             <>
                                 <label htmlFor="weight" >Weight:</label>
-                                <input type="number" onChange={handleChange} onClick={() => onClick('weight')} className={inputStyle('weight')} name="weight" id="weight" />
+                                <input type="number" onChange={handleChange} onClick={() => onClick('weight')} className={inputStyle('weight')} name="weight" id="#weight" />
                                 <p>Please Provide Weight in Kg!</p>
                             </>
                             : select === "DVD" ?
                                 <>
                                     <label htmlFor="size">Size:</label>
-                                    <input type="number" onChange={handleChange} onClick={() => onClick('size')} className={inputStyle('size')} name="size" id="size" />
+                                    <input type="number" onChange={handleChange} onClick={() => onClick('size')} className={inputStyle('size')} name="size" id="#size" />
                                     <p>Please Provide Size in Mb!</p>
                                 </>
                                 : select === "Furniture" ?
                                     <>
                                         <label htmlFor="height" >Height:</label>
-                                        <input type="number" onChange={handleChange} onClick={() => onClick('height')} className={inputStyle('height')} name="height" id="height" />
+                                        <input type="number" onChange={handleChange} onClick={() => onClick('height')} className={inputStyle('height')} name="height" id="#height" />
                                         <label htmlFor="width" >Width:</label>
-                                        <input type="" onChange={handleChange} onClick={() => onClick('width')} className={inputStyle('width')} name="width" id="width" />
+                                        <input type="" onChange={handleChange} onClick={() => onClick('width')} className={inputStyle('width')} name="width" id="#width" />
                                         <label htmlFor="length">Length:</label>
-                                        <input type="number" onChange={handleChange} onClick={() => onClick('length')} className={inputStyle('length')} name="length" id="length" />
+                                        <input type="number" onChange={handleChange} onClick={() => onClick('length')} className={inputStyle('length')} name="length" id="#length" />
                                         <p>Please Provide Dimensions in HxWxL!</p>
                                     </>
                                     : ""}
