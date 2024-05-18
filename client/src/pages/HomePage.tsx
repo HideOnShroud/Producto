fa0fe3c79e84818ee544cb46c2b1607abcd36334
HideOnShroud <natroshviliguga5@gmail.com>
2024-05-18 22:48:19 +0400

    Fixed

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -2,14 +2,19 @@ import { useEffect, useState } from "react";
 import Item from "../components/Item";
 import Navbar from "../components/Navbar";
 import useItem from "../store";
+import { ItemInterface } from "../entities/ItemInterface";
 
 const HomePage = () => {
     const getItems = useItem((state) => state.getItem)
     const items = useItem((state) => state.item)
-    const [iteme, setItems] = useState(items)
+    const [iteme, setItems] = useState<ItemInterface[]>()
     useEffect(() => {
         getItems()
     }, [getItems])
+    useEffect(() => {
+
+        setItems(items)
+    }, [items])
     const [select, setSelect] = useState<string[]>([])
 
     const handleClick = (sku: string) => {
@@ -37,12 +42,12 @@ const HomePage = () => {
                 <Navbar title="Product List" button1Action="/addproduct" button1="ADD" button2="MASS DELETE" button2Action={deleteSelected} />
             </div>
             <div className="flex flex-auto pt-20 flex-row bg-neutral-50 justify-center flex-wrap gap-10 pl-20 pr-20">
-                {iteme.map((item) => <div key={item.sku} className={select.includes(item.sku) ? "opacity-50 relative" : "relative"} onClick={() => handleClick(item.sku)}>
+                {iteme ? iteme.map((item) => <div key={item.sku} className={select.includes(item.sku) ? "opacity-50 relative" : "relative"} onClick={() => handleClick(item.sku)}>
                     <input type="checkbox" checked={select.includes(item.sku)} className="delete-checkbox absolute z-10 top-4 left-5 size-5" />
                     <Item item={item} />
                 </div>
                 )
-                }
+                    : ""}
 
             </div>
         </>
