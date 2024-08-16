357d3c00eeeb2b92074f0a4344dbd0fbf0c73bec
HideOnShroud <natroshviliguga5@gmail.com>
2024-08-16 20:55:11 +0400

    updated bugs

--- server/public/index.php
+++ server/public/index.php
@@ -3,6 +3,13 @@
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
+require '../vendor/autoload.php';
+
+// require_once'../src/Database/Database.php';
+// require_once'../src/Database/DatabaseInitializer.php';
+// $db = Producto\Database\Database::getInstance()->getConnection();
+// Producto\Database\DatabaseInitializer::initialize($db);
+// $db = null; // Close the connection
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     // Handle preflight request
@@ -10,7 +17,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
 }
 
 
-require '../vendor/autoload.php';
-
 $router = require '../src/Routes/routes.php';
-$router->dispatch();
+// $router->dispatch();
+// if you cannot run databaseInit.php, you can uncomment the following line to create the tables then comment it back.
