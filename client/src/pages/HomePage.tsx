cdd316494e018242e69e736b5880f8666efa0196
HideOnShroud <natroshviliguga5@gmail.com>
2024-08-21 17:04:13 +0400

    test2

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -29,7 +29,7 @@ const HomePage = () => {
 
     const deleteSelected = async () => {
         await Promise.all(select.map(sku => deleteItem(sku))).then(() => getItems())
-        setSelect([])
+        // setSelect([])
     }
 
 
